c7299c5438d6fe4ecd35a1cfe8b70d76a7f559d7
dmunozva <Dani@192.168.1.48>
2014-03-24 09:17:49 +0100

    modificación para ver si rula

--- GestionAula/src/es/isolutio/gestionaula/MainActivity.java
+++ GestionAula/src/es/isolutio/gestionaula/MainActivity.java
@@ -3,6 +3,7 @@ package es.isolutio.gestionaula;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +20,8 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void salir(View v) {
+		this.finish();
+	}
+	
 }
