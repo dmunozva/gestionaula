82bc2a30bfb4259fb0b3a18d4b080fa36019cff0
Irene Marcos Rivera <Irene@Irene1>
2014-03-24 12:51:26 +0100

    sssss

--- GestionAula/src/es/isolutio/gestionaula/MainActivity.java
+++ GestionAula/src/es/isolutio/gestionaula/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
 	}
 
 	public void salirIM(View v ){
-		//cosaaaaaaaaaaaaaaaaaaaaaa
+		//cosaaaaaaaaaaaaaaaaaaaaaas
 		this.finish();
 	}
 	public void salir(View v) {
